d763bc696f18e965b86e746a572d609bdb344f2a
otojo <otar.jojishvili.1@btu.edu.ge>
2026-09-09 15:06:42 +0400

    initialize Spring Boot project

---
+++ src/main/java/com/otarjojishvili/settlementfundingselector/SettlementFundingSelectorApplication.java
@@ -0,0 +1,13 @@
+package com.otarjojishvili.settlementfundingselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementFundingSelectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementFundingSelectorApplication.class, args);
+    }
+
+}
